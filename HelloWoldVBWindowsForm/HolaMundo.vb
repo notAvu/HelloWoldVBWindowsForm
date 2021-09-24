ae8e0ef7e4533988dc7b5baad723b11976c0dd78
Álvaro <alvaro.fernandez@iesnervion.es>
2021-09-24 13:32:17 +0200

    Agregar archivos de proyecto.

---
+++ HelloWoldVBWindowsForm/HolaMundo.vb
@@ -0,0 +1,16 @@
+﻿Public Class HolaMundo
+    ''' <summary>
+    ''' Recogemos el texto introducido por el usuario y escribimos "Hello "+ la caddena introducida por el usuario
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim p As String
+        p = String.Concat("Hello ", TextBox1.Text)
+        Dim v As Boolean = TextBox1.Text.Length > 0
+        If v Then
+            MessageBox.Show(p, "Hello world")
+        End If
+    End Sub
+End Class

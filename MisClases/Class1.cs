d1e08ba2608ebfd7be75391c8a27cb2226217b8e
Álvaro <alvaro.fernandez@iesnervion.es>
2021-10-15 12:10:35 +0200

    Añadida clase clsListado

---
+++ MisClases/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MisClases
+{
+    class clsListado
+    {
+        List<clsPersona> personas;
+
+        public clsListado()
+        {
+            RellenarPersonas();
+        }
+
+        public List<clsPersona> Personas { get => personas; set => personas = value; }
+
+        private void RellenarPersonas() 
+        {
+            personas.Add(new clsPersona("Fernando Galiana"));
+            personas.Add(new clsPersona("Fray Guillermo"));
+            personas.Add(new clsPersona("Yo"));
+            personas.Add(new clsPersona("Edgard Frank Codd"));
+            personas.Add(new clsPersona("Perro Samxez"));
+            personas.Add(new clsPersona("Bilbo Baggins"));
+            personas.Add(new clsPersona("Manolo"));
+        }
+        public void AddPersonas(string personaName) 
+        {
+            personas.Add(new clsPersona(personaName));
+        }
+    }
+}
